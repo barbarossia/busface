73943ca48c0dfed268dc431adbd66b9c6cb150ca
barbarossia <barbarossia.gol@gmail.com>
2019-10-23 18:01:49 +0800

    add train

--- busface/model/classify.py
+++ busface/model/classify.py
@@ -1,63 +1,48 @@
 import numpy as np
 from sklearn import svm
 from sklearn import manifold
-from sklearn.externals import joblib
-from busface.util import get_cwd
+from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score, confusion_matrix
+from busface.util import logger, get_data_path, MODEL_PATH, APP_CONFIG
 import os
 from sklearn.decomposition import PCA
 import numpy as np
 
-class Classify:
-    X = np.zeros(2, )
-    y = np.zeros(5)
-    clsf = None
-    tsne = None
-    path = "data\model"
-    model_file = os.path.join(get_cwd(), path, "train.mdl")
+MODEL_FILE = MODEL_PATH + 'train.mdl'
+MIN_TRAIN_NUM = int(APP_CONFIG['sample.n_components'])
+
+def create_model():
+    knn = KNeighborsClassifier(n_neighbors=11)
+    return knn
+
+def train():
+    model = create_model()
+    X_train, X_test, y_train, y_test = prepare_data()
+    total = len(X_test) + len(X_train)
+    if total < MIN_TRAIN_NUM:
+        raise ValueError(f'训练数据不足, 无法训练模型. 需要{MIN_TRAIN_NUM}, 当前{total}')
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    confusion_mtx = confusion_matrix(y_test, y_pred)
+    scores = evaluate(confusion_mtx, y_test, y_pred)
+    models_data = (model, scores)
+    dump_model(get_data_path(MODEL_FILE), models_data)
+    logger.info('new model trained')
+    return models_data
 
 
-
-    def __init__(self):
-        self.clsf = svm.SVC(gamma='scale')
-        self.tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
-
-    def setTrainData(self, trainData):
-    #     for t in trainData:
-    #         self.X.append(t)
-        self.X = trainData
-
-    def setTypeData(self, typeData):
-        #for t in typeData:
-        #    self.y.append(t)
-        self.y = typeData
-
-    # 降维处理
-    def dimentionTransform(self, faceData, isTrain=False):
-        newX = []
-        faceCnt = 0
-        n_components = 150
-        pca = PCA(n_components=n_components, svd_solver='randomized',
-                  whiten=True).fit(faceData)
-        newX = pca.transform(faceData)
-        return newX
-
-    def train(self):
-        if len(self.y) < 2:
-            print("样本少于 2 个，请继续选择")
-            return
-        print("开始训练")
-
-        # 先将 32*32 图形矩阵降维至 2*32，然后计算两列的平均值之后再分类
-        newX = self.dimentionTransform(self.X)
-        self.clsf.fit(newX, self.y)
-
-    def chkType(self, faceData):
-        clsf = joblib.load(self.model_file)
-        newFaces = self.dimentionTransform(faceData)
-        arr = clsf.predict(newFaces)
-        return arr
-
-    def saveModule(self):
-        print("开始保存模型")
-        joblib.dump(self.clsf, self.model_file)
-        print("保存结束")
+def evaluate(confusion_mtx, y_test, y_pred):
+    tn, fp, fn, tp = confusion_mtx.ravel()
+    # accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    logger.info(f'tp: {tp}, fp: {fp}')
+    logger.info(f'fn: {fn}, tn: {tn}')
+    # logger.info(f'accuracy_score: {accuracy}')
+    logger.info(f'precision_score: {precision}')
+    logger.info(f'recall_score: {recall}')
+    logger.info(f'f1_score: {f1}')
+    model_scores = dict(precision=precision, recall=recall, f1=f1)
+    model_scores = {key: float('{:.2f}'.format(value))
+                    for key, value in model_scores.items()}
+    return model_scores
